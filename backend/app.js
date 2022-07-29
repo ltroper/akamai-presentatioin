8ae7e0f44629663f1d68324169c2c3da080039f1
Leon Troper <ltroper@gmail.com>
2022-07-28 23:27:14 -0600

    created react-app

--- backend/app.js
+++ backend/app.js
@@ -9,5 +9,23 @@ const app = express();
 app.use(routes);
 
 
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+    });
+});
 
 module.exports = app;
