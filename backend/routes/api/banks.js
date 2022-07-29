39dfb9d57f08debed1f1fca1ac29bcb50572f231
Leon Troper <ltroper@gmail.com>
2022-07-29 10:49:09 -0600

    added bank array

--- backend/routes/api/banks.js
+++ backend/routes/api/banks.js
@@ -2,7 +2,7 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const axios = require('axios')
-const request = require("request")
+const request = require('request');
 const cheerio = require('cheerio')
 
 const router = express.Router();
@@ -14,24 +14,22 @@ const url = "https://gee.bccr.fi.cr/IndicadoresEconomicos/Cuadros/frmConsultaTCV
 
 router.post("/", asyncHandler (async (req, res) => {
     const bank = req.body
+    let banksArr = []
 
-    console.log(bank)
     request(url, (err, res, html) => {
         if (!err && res.statusCode == 200){
             const $ = cheerio.load(html)
-            const table = $("#DG")
+            // const table = $("#DG")
+            // const thisBank = table.find(bank).text()
 
-            const thisBank = table.find(bank).text()
+            const banks = $('#DG > tbody > tr').text()
+            banksArr.push(banks)
+            console.log(banksArr)
             // console.log(table.text())
             // console.log(thisBank)
         }
     })
-    // axios(url).then(res => {
-    //     let html = res.data
-    //     let $ = cheerio.load(html)
-    //     $(bank, html)
-    // })
-    return "hello"
+    return res.json(banksArr)
 }))
 
 
